cc33e5f124dacc9105710e8ebfa8d18110b9b4ed
Gerardo <lucaprodan@fibertel.com.ar>
2014-12-02 22:51:52 -0300

    v1.1

--- js/mio.js
+++ js/mio.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
 
+	$('a').click(function(){
+	$('html, body').animate({
+	    scrollTop: $( $.attr(this, 'href') ).offset().top
+	}, 700);
+	    return false;
+	});  
+
+
 });
